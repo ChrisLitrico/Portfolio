2a9d05e02f6ea55b2b323b0cc87cf7390ef7286a
Umamitas <umamitas.twitch@gmail.com>
2023-11-30 09:43:49 +0100

    aggiunto logo 3d con Spline e reso interattivo con l'hovering. Inoltre ho aggiunto anche la sezione portfolio con l'ecommerce creato da me

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -4,8 +4,10 @@ import Spline from '@splinetool/react-spline'
 const Logo = () => {
   return (
     <div>
-         <Spline className='logo-container' scene="https://prod.spline.design/YQODGvnoEkdfOUfs/scene.splinecode" />
-  
+      <Spline
+        className="logo-container"
+        scene="https://prod.spline.design/YQODGvnoEkdfOUfs/scene.splinecode"
+      />
     </div>
   )
 }
